1d6dc6405745cd1ddedd1f07329706ed7cee4617
sanjeev0901 <76206355+sanjeev0901@users.noreply.github.com>
2022-03-01 19:17:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC09oPHEAC0LClDqBEMmmeu91m8BRhku58",
+  authDomain: "storekeeperapp-f63b6.firebaseapp.com",
+  databaseURL: "https://storekeeperapp-f63b6-default-rtdb.firebaseio.com",
+  projectId: "storekeeperapp-f63b6",
+  storageBucket: "storekeeperapp-f63b6.appspot.com",
+  messagingSenderId: "663437957887",
+  appId: "1:663437957887:web:45cfa30d71bd99c733caca"
+};
